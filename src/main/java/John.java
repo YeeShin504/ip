7eaa3e04e144cdc7b38aaf7321be8c7155a2a888
ys_teng <yeeshin13@gmail.com>
2026-01-14 22:29:37 +0800

    feat: changed bot name to John

---
+++ src/main/java/John.java
@@ -0,0 +1,23 @@
+public class John {
+    public static String linebreak = "_".repeat(100);
+
+    public static void main(String[] args) {
+        String logo = "       __      __        \n"
+                    + "      / /___  / /_  ____\n"
+                    + " __  / / __ \\/ __ \\/ __ \\\n"
+                    + "/ /_/ / /_/ / / / / / / / \n"
+                    + "\\____/\\____/_/ /_/_/ /_/\n";
+
+        System.out.println(linebreak);
+        System.out.println("Hello! I'm\n" + logo);
+        System.out.println("What can I do for you?");
+        System.out.println(linebreak);
+
+        exit();
+    }
+
+    public static void exit() {
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(linebreak);
+    }
+}
