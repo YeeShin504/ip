054158b4387a1f7c6fa677174d6203b10ba76b1b
ys_teng <yeeshin13@gmail.com>
2026-01-15 11:57:46 +0800

    feat: add ability to mark tasks

--- src/main/java/John.java
+++ src/main/java/John.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class John {
-    private static final ArrayList<String> userList = new ArrayList<>();
+    private static final ArrayList<Task> tasks = new ArrayList<>();
 
     public static void main(String[] args) {
         String logo = """
@@ -20,9 +20,9 @@ public class John {
         Scanner scanner = new Scanner(System.in);
         boolean run = true;
         while (run) {
-            String input = scanner.nextLine().toLowerCase();
+            String[] input = scanner.nextLine().toLowerCase().split(" ");
             linebreak();
-            switch (input) {
+            switch (input[0]) {
                 case "list":
                     printList();
                     break;
@@ -31,22 +31,49 @@ public class John {
                     scanner.close();
                     exit();
                     break;
+                case "mark":
+                    markComplete(input[1]);
+                    break;
+                case "unmark":
+                    markIncomplete(input[1]);
+                    break;
                 default:
-                    addToList(input);
+                    addToList(String.join(" ", input));
             }
             linebreak();
         }
     }
 
     private static void addToList(String input) {
-        userList.add(input);
+        tasks.add(new Task(input));
         System.out.printf("added: %s\n", input);
     }
 
     private static void printList() {
-        for (int i = 0; i < userList.size(); i++) {
-            System.out.printf("%s. %s\n", (i + 1), userList.get(i));
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.printf("%s. %s\n", (i + 1), tasks.get(i));
         }
+    }
+
+    private static void markComplete(String taskNum) {
+        int idx = Integer.parseInt(taskNum) - 1;
+        if (idx < 0 || idx >= tasks.size()) {
+            System.out.println("Error: Invalid task number");
+            return;
+        }
+        Task task = tasks.get(idx);
+        task.markComplete();
+    }
+
+    private static void markIncomplete(String taskNum) {
+        int idx = Integer.parseInt(taskNum) - 1;
+        if (idx < 0 || idx >= tasks.size()) {
+            System.out.println("Error: Invalid task number");
+            return;
+        }
+        Task task = tasks.get(idx);
+        task.markIncomplete();
     }
 
     private static void exit() {
---
+++ src/main/java/Task.java
@@ -0,0 +1,34 @@
+public class Task {
+    protected String description;
+    protected boolean isComplete = false;
+
+    public Task(String description) {
+        this.description = description;
+        this.isComplete = false;
+    }
+
+    public String toString() {
+        String status = isComplete ? "[X]" : "[ ]";
+        return String.format("%s %s", status, description);
+    }
+
+    protected void markComplete() {
+        if (isComplete) {
+            System.out.println("This task has already been marked as done:");
+        } else {
+            isComplete = true;
+            System.out.println("Nice! I've marked this task as done:");
+            System.out.printf("    %s\n", this);
+        }
+    }
+
+    protected void markIncomplete() {
+        if (!isComplete) {
+            System.out.println("This task has already been marked as not done:");
+        } else {
+            isComplete = false;
+            System.out.println("OK, I've marked this task as not done yet:");
+        }
+        System.out.printf("    %s\n", this);
+    }
+}
