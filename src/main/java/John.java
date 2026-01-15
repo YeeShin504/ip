ab58fe0dafb57ba389520afeaa307ef349ab628a
ys_teng <yeeshin13@gmail.com>
2026-01-15 10:58:41 +0800

    feat: added user's list

--- src/main/java/John.java
+++ src/main/java/John.java
@@ -1,36 +1,59 @@
-import java.util.Objects;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class John {
-    public static String linebreak = "_".repeat(100);
+    private static final ArrayList<String> userList = new ArrayList<>();
 
     public static void main(String[] args) {
-        String logo = "       __      __        \n"
-                    + "      / /___  / /_  ____\n"
-                    + " __  / / __ \\/ __ \\/ __ \\\n"
-                    + "/ /_/ / /_/ / / / / / / / \n"
-                    + "\\____/\\____/_/ /_/_/ /_/\n";
-
-        System.out.println(linebreak);
+        String logo = """
+                       __      __       \s
+                      / /___  / /_  ____
+                 __  / / __ \\/ __ \\/ __ \\
+                / /_/ / /_/ / / / / / / /\s
+                \\____/\\____/_/ /_/_/ /_/
+                """;
+        linebreak();
         System.out.println("Hello! I'm\n" + logo);
         System.out.println("What can I do for you?");
-        System.out.println(linebreak);
+        linebreak();
 
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        while (!input.equals("bye")) {
-            System.out.println(linebreak);
-            System.out.println(input);
-            System.out.println(linebreak);
-            input = scanner.nextLine();
+        boolean run = true;
+        while (run) {
+            String input = scanner.nextLine().toLowerCase();
+            linebreak();
+            switch (input) {
+                case "list":
+                    printList();
+                    break;
+                case "bye":
+                    run = false;
+                    scanner.close();
+                    exit();
+                    break;
+                default:
+                    addToList(input);
+            }
+            linebreak();
         }
-
-        scanner.close();
-        exit();
     }
 
-    public static void exit() {
+    private static void addToList(String input) {
+        userList.add(input);
+        System.out.printf("added: %s\n", input);
+    }
+
+    private static void printList() {
+        for (int i = 0; i < userList.size(); i++) {
+            System.out.printf("%s. %s\n", (i + 1), userList.get(i));
+        }
+    }
+
+    private static void exit() {
         System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(linebreak);
+    }
+
+    private static void linebreak() {
+        System.out.println("_".repeat(100));
     }
 }
